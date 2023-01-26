7f861bff032902b5a66d9572c93947ed043d2a1c
sks8239 <100519915+sks8239@users.noreply.github.com>
2023-01-26 19:48:11 +0900

    Update WordStudy.java

--- src/단어공부/WordStudy.java
+++ src/단어공부/WordStudy.java
@@ -7,15 +7,24 @@ public class WordStudy {
         Scanner sc = new Scanner(System.in);
         System.out.print("단어를 입력하세요 : ");
         String word = sc.next();
-        int alphabet[] = new int[word.length()];
-        word.toLowerCase();
+        int alphabet[] = new int[26];
+        int max = 0;
+        char out = '?';
         for(int i=0; i<word.length(); i++) {
-            for(int j=1; j<word.length(); j++){ // s k s a b c
-                if(word.charAt(i)==word.charAt(j)){
-                }
+            if('A' <= word.charAt(i) && word.charAt(i)<= 'Z'){ //대문자 구간
+                alphabet[word.charAt(i) - 'A']++;
+            }else{ // 소문자 구간
+                alphabet[word.charAt(i) - 'a']++;
             }
-            System.out.println(alphabet[i]);
         }
-
+        for(int i=0; i<26; i++){
+            if(alphabet[i]>max){
+                max = alphabet[i];
+                out = (char)(i+65); // 대문자 출력 +65
+            }else if (alphabet[i] == max){
+                out = '?';
+            }
+        }
+        System.out.println(out);
     }
 }
